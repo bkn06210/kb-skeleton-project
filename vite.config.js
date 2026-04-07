a1f365a1f430d984888a8bae4ddd2de6071a0009
flyjunu <96277486+flyjunu@users.noreply.github.com>
2026-04-07 15:48:33 +0900

    Remove vueDevTools from Vite configuration
    실수로 import문 한줄이 빠져 있어서, 한 줄만 추가할게요.
    미안합니다.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), 
-            vueDevTools()
-           ], 
+  plugins: [vue(),], 
   
   resolve: {
     alias: {

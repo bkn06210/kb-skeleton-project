f950d7b81c193a8ac92c0c66acf986d4f2332214
awnsdna <awnsdna@gmail.com>
2026-04-07 14:57:18 +0900

    라우터 초기세팅

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/Views/HomeView.vue'
+import TransactionView from '@/Views/TransactionView.vue'
+import FormView from '@/Views/FormView.vue'
+import SettingsView from '@/Views/SettingsView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/transactions',
+    name: 'transactions',
+    component: TransactionView
+  },
+  {
+    path: '/add',
+    name: 'add',
+    component: FormView // 새 거래 등록
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit',
+    component: FormView // 기존 거래 수정 (동일 컴포넌트 재활용)
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: SettingsView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router

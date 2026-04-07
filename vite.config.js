a5593e131ea60ec757239fd3b3e3a66019d78335
awnsdna <awnsdna@gmail.com>
2026-04-07 15:07:30 +0900

    프록시설정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), 
+            vueDevTools()
+           ], 
+  
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        },
+    },
 })
